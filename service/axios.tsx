a741f6fc18efa0e7bc1e341ea0ef5139bebbea33
markothedev <markothedevmail@gmail.com>
2023-02-18 21:29:03 +0100

    updated the access token error

--- service/axios.tsx
+++ service/axios.tsx
@@ -40,7 +40,10 @@ api.interceptors.response.use(function (response) {
   try{
     const code:any = error.response.data.messages[0]
   if(code.token_class === 'AccessToken') {
-    if (!window.location.pathname.includes('/expired_token_page' )|| !window.location.pathname.includes('/')|| !window.location.pathname.includes('/sigin')|| !window.location.pathname.includes('/sigup')){
+    if (!window.location.pathname.includes('/expired_token_page' )|| !window.location.pathname.includes('/')|| !window.location.pathname.includes('/sigin')
+    || !window.location.pathname.includes('/sigup'))
+    || !window.location.pathname.includes('/job_seeker_signup'))
+    {
 
       window.location.reload()
       window.location.href='/expired_token_page'
